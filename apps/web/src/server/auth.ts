dffa1506d1b056131c626ac6b30e19ee1f4e2035
Luka Hietala <95122845+LukaHietala@users.noreply.github.com>
2023-03-01 11:13:23 +0000

    fixed imports

--- apps/web/src/server/auth.ts
+++ apps/web/src/server/auth.ts
@@ -7,7 +7,7 @@ import {
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
-import { prisma } from "~/server/db";
+import { prisma } from "database";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
