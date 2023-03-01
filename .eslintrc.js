3600cdacedd71ec9b6a97ae437b930738fdff5bd
Luka Hietala <luka.hietala08@gmail.com>
2023-03-01 19:59:49 +0200

    MANY new changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,22 @@
-module.exports = {
-	root: true,
-	// This tells ESLint to load the config from the package `eslint-config-custom`
-	extends: ['custom'],
-	settings: {
-		next: {
-			rootDir: ['apps/*/'],
-		},
-	},
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  extends: ["custom"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    tsconfigRootDir: __dirname,
+    project: [
+      "./tsconfig.json",
+      "./apps/*/tsconfig.json",
+      "./packages/*/tsconfig.json",
+    ],
+  },
+  settings: {
+    next: {
+      rootDir: ["apps/web"],
+    },
+  },
 };
+
+module.exports = config;
