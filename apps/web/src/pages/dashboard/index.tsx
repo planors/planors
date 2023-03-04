a9829bb3caff378dad958beb50f314f3a7d53006
Luka Hietala <luka.hietala08@gmail.com>
2023-03-04 13:37:40 +0200

    small notification if you dont have a wikis yet

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Inter } from "@next/font/google";
 import { useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
+import { Info } from "~/common/svg";
 
+const inter = Inter({
+  preload: false,
+  subsets: ["latin"],
+});
 export default function Dashboard() {
   const { data: session, status } = useSession();
 
@@ -42,8 +48,31 @@ export default function Dashboard() {
   }
 
   return (
-    <main>
-      <h1>Dashboard</h1>
+    <main className="mx-auto my-12 max-w-5xl text-zinc-900" style={inter.style}>
+      <h1 className="text-xl font-semibold">Dashboard</h1>
+      {data && data.length === 0 && (
+        <div className="mt-4 rounded-md border border-gray-200 bg-zinc-50 p-5">
+          <div className="flex w-full flex-row gap-4">
+            <Info className="h-10 w-10 text-orange-300" />
+            <div className="flex flex-col">
+              <h2 className="mb-1 font-semibold">No wikis found</h2>
+              <p className="text-sm text-zinc-600">
+                You don't seem to have any wiki(s) yet. You can create one by
+                clicking the button below. If you already have a wiki, you can
+                go to the import page to import it (coming soon)
+              </p>
+              <div className="mt-5">
+                <Link
+                  href="/create"
+                  className="rounded-md border border-zinc-300 bg-white py-2 px-3 text-sm font-medium text-zinc-800 transition-all duration-100 ease-in-out"
+                >
+                  Create
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <Image
         src={session?.user?.image as string}
         width={64}
