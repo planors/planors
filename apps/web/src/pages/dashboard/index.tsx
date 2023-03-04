02e1ece96690f67715170e022b5ff3e971240cdf
Luka Hietala <luka.hietala08@gmail.com>
2023-03-04 18:19:59 +0200

    ui changes

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -50,8 +50,11 @@ export default function Dashboard() {
   return (
     <main className="mx-auto my-12 max-w-5xl text-zinc-900" style={inter.style}>
       <h1 className="text-xl font-semibold">Dashboard</h1>
+      <p className="mt-2 mb-8 text-zinc-700">
+        Manage, create and edit your wiki(s) here
+      </p>
       {data && data.length === 0 && (
-        <div className="mt-4 rounded-md border border-gray-200 bg-zinc-50 p-5">
+        <div className="select-none rounded-md border border-zinc-200 bg-zinc-50 p-5">
           <div className="flex w-full flex-row gap-4">
             <Info className="h-10 w-10 text-orange-300" />
             <div className="flex flex-col">
@@ -61,47 +64,89 @@ export default function Dashboard() {
                 clicking the button below. If you already have a wiki, you can
                 go to the import page to import it (coming soon)
               </p>
-              <div className="mt-5">
+              <div className="mt-5 flex flex-row gap-2">
                 <Link
                   href="/create"
                   className="rounded-md border border-zinc-300 bg-white py-2 px-3 text-sm font-medium text-zinc-800 transition-all duration-100 ease-in-out"
                 >
                   Create
                 </Link>
+                <button
+                  className="rounded-md border border-zinc-300 bg-white py-2 px-3 text-sm font-medium text-zinc-800 transition-all duration-100 ease-in-out "
+                  type="button"
+                >
+                  Dismiss
+                </button>
               </div>
             </div>
           </div>
         </div>
       )}
-      <Image
-        src={session?.user?.image as string}
-        width={64}
-        height={64}
-        alt={session?.user?.name as string}
-        className="rounded-full"
-        unoptimized
-      />
-      <h2>
-        {session?.user?.name} - {session?.user?.email}
-      </h2>
-      <h3>Your Wikis</h3>
-      {isLoading && <div>loading...</div>}
+      {isLoading && (
+        <div className="mt-5 text-zinc-800">Loading your wiki(s)...</div>
+      )}
       {error && <div>error: {error.message}</div>}
-      {data?.map((wiki) => (
-        <div
-          key={wiki.id}
-          className="flex flex-row items-center justify-between py-4"
-        >
-          <Link href={`dashboard/${wiki.id}`}>
-            <h4>{wiki.title}</h4>
-          </Link>
-          <button key={wiki.id} onClick={() => handleDelete(wiki.id)}>
-            Delete
-          </button>
-          {mutationError && <div>error: {mutationError.message}</div>}
-          {isMutating && <div>deleting...</div>}
-        </div>
-      ))}
+      {data && data.length > 0 && (
+        <section>
+          <div className="mt-4 select-none rounded-md border border-zinc-200 bg-zinc-50 p-5">
+            <div className="flex w-full flex-row gap-4">
+              <Info className="h-10 w-10 text-orange-300" />
+              <div className="flex flex-col">
+                <h2 className="mb-1 font-semibold">
+                  You're in the development version
+                </h2>
+                <p className="text-sm text-zinc-600">
+                  This is a development version of the app, so some features may
+                  not work as expected. If you find any bugs, please report them
+                  on the GitHub issues of the project.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-4 rounded-md border border-zinc-200">
+            <div className="flex flex-row items-center justify-between border-b border-zinc-200 py-2 px-4">
+              <h2 className="text-sm font-semibold">Your wiki(s)</h2>
+              <Link
+                href="/create"
+                className="rounded-md border border-zinc-200 bg-white py-2 px-3 text-sm font-medium text-zinc-800 transition-all duration-100 ease-in-out"
+              >
+                Create
+              </Link>
+            </div>
+            <div className="divide-y ">
+              {data?.map((wiki) => (
+                <div
+                  key={wiki.id}
+                  className="group flex flex-row items-center justify-between py-3 px-4 hover:bg-zinc-50"
+                >
+                  <Link href={`dashboard/${wiki.id}`} className="text-sm">
+                    <h4>{wiki.title}</h4>
+                  </Link>
+                  <div className="flex flex-row gap-2">
+                    <button
+                      key={wiki.id}
+                      className="invisible rounded-md border border-zinc-200 bg-white py-2 px-2 text-sm font-medium text-zinc-800 transition-all duration-100 ease-in-out group-hover:visible"
+                    >
+                      Manage
+                    </button>
+                    <button
+                      key={wiki.id}
+                      className="invisible rounded-md border border-zinc-200 bg-white py-2 px-2 text-sm font-medium text-zinc-800 transition-all duration-100 ease-in-out group-hover:visible"
+                      onClick={() => handleDelete(wiki.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+
+                  {mutationError && <div>error: {mutationError.message}</div>}
+                  {isMutating && <div>deleting...</div>}
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
