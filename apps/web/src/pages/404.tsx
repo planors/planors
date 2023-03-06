00c21a2347536abe91fbd846aafba90a9e2f2612
Luka Hietala <luka.hietala08@gmail.com>
2023-03-06 20:35:50 +0200

    styling changes

--- apps/web/src/pages/404.tsx
+++ apps/web/src/pages/404.tsx
@@ -10,7 +10,7 @@ export default function Page404() {
   return (
     <main style={inter.style} className="mx-auto my-12 max-w-xl text-zinc-900">
       <h1 className="text-center text-2xl font-bold">404 - Page Not Found</h1>
-      <p className="text-center text-zinc-800">
+      <p className="mt-1 text-center text-zinc-800">
         Sorry, we couldn&apos;t find the page you were looking for
       </p>
       <h2 className="mt-8 mb-1 text-sm font-medium text-zinc-700">
@@ -58,10 +58,12 @@ export default function Page404() {
         </Link>
       </div>
       <hr className="mb-4 mt-8 border-zinc-200" />
-      <p className="text-center text-zinc-800">
+      <p className="text-center text-sm text-zinc-800">
         If you think this is a bug, please report it on{" "}
         <Link
           href="https://github.com/LukaHietala/planors/issues"
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-blue-500"
         >
           GitHub
